512a0372470daba1eb779de0cef02d550d867540
Janai Kameka <jkameka@fb.com>
2020-06-25 18:31:38 -0400

    Tap a cell and see detail view

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Janai Kameka on 6/25/20.
+//  Copyright © 2020 Janai Kameka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
